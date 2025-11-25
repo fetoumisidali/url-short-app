2b277bbab77a6bd8675acb17194e9de6bd4db8bd
Sidali Fetoumi <fetoumisidali@gmail.com>
2025-11-25 03:36:19 +0100

    disable auto config

--- short-api/src/main/java/com/sidalifetoumi/shorter/UrlShorterApplication.java
+++ short-api/src/main/java/com/sidalifetoumi/shorter/UrlShorterApplication.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.cache.interceptor.CacheableOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -13,6 +15,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
 public class UrlShorterApplication {
 
 	public static void main(String[] args) {
